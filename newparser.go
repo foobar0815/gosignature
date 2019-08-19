f50bf22d791d8bd4a7cb63c5d3bd982f9c7bbe63
foobar0185 <atalanta.bergamo@gmail.com>
2019-08-19 20:32:03 +0200

    Change delimiter to double brackets

--- newparser.go
+++ newparser.go
@@ -2,19 +2,12 @@ package main
 
 import (
 	"bytes"
-	"strings"
 	"text/template"
 )
 
 func newParser(fieldMap map[string]string, templateName, tmpl, extension string) string {
-	// HACK?
-	if extension == "rtf" {
-		tmpl = strings.Replace(tmpl, "\\{\\{", "{{", -1)
-		tmpl = strings.Replace(tmpl, "\\}\\}", "}}", -1)
-	}
-
 	buf := new(bytes.Buffer)
-	t, err := template.New(templateName).Parse(tmpl)
+	t, err := template.New(templateName).Delims("[[", "]]").Parse(tmpl)
 	checkErr(err)
 	err = t.Execute(buf, fieldMap)
 	tmpl = buf.String()
