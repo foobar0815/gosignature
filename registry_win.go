6a1a7ef74feac747f2f6fdb01c3e01e359d629b9
foobar0815 <atalanta.bergamo@gmail.com>
2019-10-03 13:35:17 +0200

    Hopefully more robust and generic approach
    including support for Outlook 2010 (Office 12)

--- registry_win.go
+++ registry_win.go
@@ -10,40 +10,112 @@ import (
 )
 
 func setSignature(signature, style, profile string, setforall int) error {
-	name := ""
-	if style == "new" {
-		name = "New Signature"
-	} else if style == "reply" {
-		name = "Reply-Forward Signature"
-	}
 	outlookVersion, _ := getOutlookVersion()
-	if outlookVersion > 14 {
+
+	if outlookVersion > 12 {
+
+		name := ""
+		if style == "new" {
+			name = "New Signature"
+		} else if style == "reply" {
+			name = "Reply-Forward Signature"
+		}
+
+		profiles := []string{}
 		if profile != "" {
-			setHkcuString(`Software\Microsoft\Office\`+strconv.Itoa(outlookVersion)+`.0\Outlook\Profiles\`+profile+`\9375CFF0413111d3B88A00104B2A6676\00000002`, name, signature)
+			profiles = append(profiles, profile)
 		} else if setforall == 1 {
-			outlookProfiles, _ := getOutlookProfiles(strconv.Itoa(outlookVersion) + ".0")
+			outlookProfiles, _ := getOutlookProfiles(outlookVersion)
 			for _, op := range outlookProfiles {
-				setHkcuString(`Software\Microsoft\Office\`+strconv.Itoa(outlookVersion)+`.0\Outlook\Profiles\`+op+`\9375CFF0413111d3B88A00104B2A6676\00000002`, name, signature)
+				profiles = append(profiles, op)
 			}
 		} else {
-			defaultProfile, _ := getOutlookDefaultProfile(strconv.Itoa(outlookVersion) + ".0")
-			setHkcuString(`Software\Microsoft\Office\`+strconv.Itoa(outlookVersion)+`.0\Outlook\Profiles\`+defaultProfile+`\9375CFF0413111d3B88A00104B2A6676\00000002`, name, signature)
+			defaultProfile, _ := getOutlookDefaultProfile(outlookVersion)
+			profiles = append(profiles, defaultProfile)
+		}
+
+		for _, p := range profiles {
+			subkeys, err := getAccountSubkeys(outlookVersion, p)
+			if err != nil {
+				return err
+			}
+			for _, sk := range subkeys {
+				setSignatureValue(outlookVersion, sk, name, signature)
+			}
 		}
 	}
 
 	return nil
 }
 
-func setHkcuString(key, name, data string) error {
+func getAccountSubkeys(outlookVersion int, profile string) ([]string, error) {
+	key := ""
+	if outlookVersion > 14 {
+		key = `Software\Microsoft\Office\` + strconv.Itoa(outlookVersion) + `.0\Outlook\Profiles\` + profile + `\9375CFF0413111d3B88A00104B2A6676`
+	} else if outlookVersion > 12 {
+		key = `Software\Microsoft\Windows NT\CurrentVersion\Windows Messaging Subsystem\Profiles\` + profile + `\9375CFF0413111d3B88A00104B2A6676`
+	}
+
+	k, err := registry.OpenKey(registry.CURRENT_USER, key, registry.READ)
+	if err != nil {
+		return nil, err
+	}
+	defer k.Close()
+
+	keystats, err := k.Stat()
+	if err != nil {
+		return nil, err
+	}
+
+	subkeycount := int(keystats.SubKeyCount)
+
+	subkeys, err := k.ReadSubKeyNames(subkeycount)
+	if err != nil {
+		return nil, err
+	}
+
+	accountSubkeys := []string{}
+	for _, subkey := range subkeys {
+		sk, err := registry.OpenKey(registry.CURRENT_USER, key+`\`+subkey, registry.READ)
+		if err != nil {
+			return nil, err
+		}
+		defer sk.Close()
+
+		mapiprovider, _, err := sk.GetIntegerValue("MAPI Provider")
+
+		if mapiprovider != 2 && mapiprovider != 4 {
+			accountSubkeys = append(accountSubkeys, key+`\`+subkey)
+		}
+	}
+
+	return accountSubkeys, nil
+
+}
+
+func setSignatureValue(outlookVersion int, key, name, value string) error {
 	k, err := registry.OpenKey(registry.CURRENT_USER, key, registry.SET_VALUE)
 	if err != nil {
 		return err
 	}
 	defer k.Close()
 
-	err = k.SetStringValue(name, data)
-	if err != nil {
-		return err
+	if outlookVersion > 14 {
+		err = k.SetStringValue(name, value)
+		if err != nil {
+			return err
+		}
+	} else if outlookVersion > 12 {
+		msValue := ""
+		for _, char := range msValue {
+			msValue += string(char) + "\x00"
+		}
+		msValue += "\x00\x00"
+
+		err = k.SetBinaryValue(name, []byte(msValue))
+		if err != nil {
+			return err
+		}
 	}
 
 	return nil
@@ -71,8 +143,15 @@ func getOutlookVersion() (int, error) {
 	return version, nil
 }
 
-func getOutlookDefaultProfile(outlookVersion string) (string, error) {
-	k, err := registry.OpenKey(registry.CURRENT_USER, `Software\Microsoft\Office\`+outlookVersion+`\Outlook`, registry.QUERY_VALUE)
+func getOutlookDefaultProfile(outlookVersion int) (string, error) {
+	key := ""
+	if outlookVersion > 14 {
+		key = `Software\Microsoft\Office\` + strconv.Itoa(outlookVersion) + `.0\Outlook`
+	} else if outlookVersion > 12 {
+		key = `Software\Microsoft\Windows NT\CurrentVersion\Windows Messaging Subsystem\Profiles`
+	}
+
+	k, err := registry.OpenKey(registry.CURRENT_USER, key, registry.QUERY_VALUE)
 	if err != nil {
 		return "", err
 	}
@@ -86,8 +165,15 @@ func getOutlookDefaultProfile(outlookVersion string) (string, error) {
 	return profile, nil
 }
 
-func getOutlookProfiles(outlookVersion string) ([]string, error) {
-	k, err := registry.OpenKey(registry.CURRENT_USER, `Software\Microsoft\Office\`+outlookVersion+`\Outlook\Profiles`, registry.READ)
+func getOutlookProfiles(outlookVersion int) ([]string, error) {
+	key := ""
+	if outlookVersion > 14 {
+		key = `Software\Microsoft\Office\` + strconv.Itoa(outlookVersion) + `.0\Outlook\Profiles`
+	} else if outlookVersion > 12 {
+		key = `Software\Microsoft\Windows NT\CurrentVersion\Windows Messaging Subsystem\Profiles`
+	}
+
+	k, err := registry.OpenKey(registry.CURRENT_USER, key, registry.READ)
 	if err != nil {
 		return nil, err
 	}
