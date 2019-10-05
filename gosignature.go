82d7d2ddcfff156dc10db163968bc113880aa709
foobar0185 <atalanta.bergamo@gmail.com>
2019-10-05 12:42:44 +0200

    Add support for "FixedSignTypeNoMobile"
    and "FixedSignTypeReplyNoMobile"

--- gosignature.go
+++ gosignature.go
@@ -61,6 +61,20 @@ func main() {
 	sd.nodefault = cfg.Section("Main").Key("NoReplyMessageSignature").MustInt(0)
 	signatureDefintions = append(signatureDefintions, sd)
 
+	sd = new(signatureDefinition)
+	sd.templateName = cfg.Section("Main").Key("FixedSignTypeNoMobile").String()
+	sd.style = "new"
+	sd.nodefault = 1
+	sd.signatureName = sd.templateName
+	signatureDefintions = append(signatureDefintions, sd)
+
+	sd = new(signatureDefinition)
+	sd.templateName = cfg.Section("Main").Key("FixedSignTypeReplyNoMobile").String()
+	sd.style = "reply"
+	sd.nodefault = 1
+	sd.signatureName = sd.templateName
+	signatureDefintions = append(signatureDefintions, sd)
+
 	userName := ""
 	ldapEntry := make(map[string]string)
 	if !(*testmode) && cfg.Section("Main").Key("LDAPBaseObjectDN").String() != "" {
@@ -112,57 +126,56 @@ func main() {
 		signatureDefintions[1].templateName = fieldMap["SignTypeReply"]
 	}
 
-	if signatureDefintions[0].templateName != "" || signatureDefintions[1].templateName != "" {
-		extensions := [3]string{"txt", "htm", "rtf"}
-		generated := []string{}
-		templateFolder := filepath.Join(programPath, cfg.Section("Main").Key("TemplateFolder").MustString("Vorlagen"))
-		destFolder := ""
-		if cfg.Section("Main").Key("AppDataPath").String() != "" {
-			destFolder = winExpandEnv(cfg.Section("Main").Key("AppDataPath").String())
-		} else {
-			destFolder = getDestFolder()
-		}
-		err = prepareFolder(destFolder)
-		checkErrAndExit(err)
-		if cfg.Section("Main").Key("EmptySignatureFolder").MustInt(0) == 1 && (*force || askForConfirmation("Do you really want to empty the destination directory ("+destFolder+")?")) {
-			removeContents(destFolder)
-		}
-		signatureDefintions[0].signatureName = cfg.Section("Main").Key("TargetSignType").MustString(signatureDefintions[0].templateName)
-		signatureDefintions[1].signatureName = cfg.Section("Main").Key("TargetSignTypeReply").MustString(signatureDefintions[1].templateName)
-		for _, sd := range signatureDefintions {
-			if sd.templateName != "" {
-				if !contains(generated, sd.signatureName) {
-					copyImages(templateFolder, sd.templateName, sd.signatureName, userName, destFolder)
-					for _, ex := range extensions {
-						signature, err := readTemplate(filepath.Join(templateFolder, sd.templateName+"."+ex))
+	extensions := [3]string{"txt", "htm", "rtf"}
+	generated := []string{}
+	templateFolder := filepath.Join(programPath, cfg.Section("Main").Key("TemplateFolder").MustString("Vorlagen"))
+	destFolder := ""
+	if cfg.Section("Main").Key("AppDataPath").String() != "" {
+		destFolder = winExpandEnv(cfg.Section("Main").Key("AppDataPath").String())
+	} else {
+		destFolder = getDestFolder()
+	}
+	err = prepareFolder(destFolder)
+	checkErrAndExit(err)
+	if cfg.Section("Main").Key("EmptySignatureFolder").MustInt(0) == 1 && (*force || askForConfirmation("Do you really want to empty the destination directory ("+destFolder+")?")) {
+		removeContents(destFolder)
+	}
+	signatureDefintions[0].signatureName = cfg.Section("Main").Key("TargetSignType").MustString(signatureDefintions[0].templateName)
+	signatureDefintions[1].signatureName = cfg.Section("Main").Key("TargetSignTypeReply").MustString(signatureDefintions[1].templateName)
+	for _, sd := range signatureDefintions {
+		if sd.templateName != "" {
+			if !contains(generated, sd.signatureName) {
+				copyImages(templateFolder, sd.templateName, sd.signatureName, userName, destFolder)
+				for _, ex := range extensions {
+					signature, err := readTemplate(filepath.Join(templateFolder, sd.templateName+"."+ex))
+					checkErrAndExit(err)
+					if *newparser {
+						signature, err = newParser(fieldMap, sd.templateName, signature, ex)
 						checkErrAndExit(err)
-						if *newparser {
-							signature, err = newParser(fieldMap, sd.templateName, signature, ex)
-							checkErrAndExit(err)
-						} else {
-							signature = compatParser(fieldMap,
-								cfg.Section("Main").Key("PlaceholderSymbol").MustString("@"),
-								signature,
-								ex)
-						}
-						if ex == "rtf" || ex == "txt" {
-							signature, err = charmap.Windows1252.NewEncoder().String(signature)
-							checkErrAndContinue(err)
-						}
-						err = writeSignature(destFolder, sd.signatureName, ex, signature)
-						checkErrAndExit(err)
-						generated = append(generated, sd.signatureName)
+					} else {
+						signature = compatParser(fieldMap,
+							cfg.Section("Main").Key("PlaceholderSymbol").MustString("@"),
+							signature,
+							ex)
 					}
+					if ex == "rtf" || ex == "txt" {
+						signature, err = charmap.Windows1252.NewEncoder().String(signature)
+						checkErrAndContinue(err)
+					}
+					err = writeSignature(destFolder, sd.signatureName, ex, signature)
+					checkErrAndExit(err)
+					generated = append(generated, sd.signatureName)
 				}
-				if sd.nodefault == 0 {
-					setSignature(sd.signatureName,
-						sd.style,
-						winExpandEnv(cfg.Section("Main").Key("EMailAccount").MustString("")),
-						cfg.Section("Main").Key("SetForAllEMailAccounts").MustInt(0))
-				}
+			}
+			if sd.nodefault == 0 {
+				setSignature(sd.signatureName,
+					sd.style,
+					winExpandEnv(cfg.Section("Main").Key("EMailAccount").MustString("")),
+					cfg.Section("Main").Key("SetForAllEMailAccounts").MustInt(0))
 			}
 		}
 	}
+
 }
 
 func readConfig(configFile string) (*ini.File, error) {
