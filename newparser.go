foobar0815/gosignature

commit 355ac4ea22ac48a03cb0ba3bfaf89c2fcf62af40

Implement recommendations by GolangCI

newparser.go
@@ -5,12 +5,18 @@ import (
 	"text/template"
 )
 
-func newParser(fieldMap map[string]string, templateName, tmpl, extension string) string {
+func newParser(fieldMap map[string]string, templateName, tmpl, extension string) (string, error) {
 	buf := new(bytes.Buffer)
 	t, err := template.New(templateName).Delims("[[", "]]").Parse(tmpl)
-	checkErr(err)
+	if err == nil {
+		return "", err
+	}
 	err = t.Execute(buf, fieldMap)
+	if err == nil {
+		return "", err
+	}
 	tmpl = buf.String()
 
-	return tmpl
+	return tmpl, nil
+
 }

gosignature.go
@@ -115,7 +115,8 @@ func main() {
 					signature, err := readTemplate(filepath.Join(templateFolder, sd.templateName+"."+ex))
 					checkErr(err)
 					if *newparser {
-						signature = newParser(fieldMap, sd.templateName, signature, ex)
+						signature, err = newParser(fieldMap, sd.templateName, signature, ex)
+						checkErr(err)
 					} else {
 						signature = compatParser(fieldMap,
 							cfg.Section("Main").Key("PlaceholderSymbol").MustString("@"),

helper.go
@@ -57,13 +57,11 @@ func getUsername() (string, error) {
 
 func getDestFolder() string {
 	destFolder := ""
-	err := *new(error)
 
 	if runtime.GOOS == "windows" {
 		destFolder = filepath.Join(os.Getenv("appdata"), "Microsoft", "Signatures")
 	} else {
-		destFolder, err = os.Getwd()
-		checkErr(err)
+		destFolder, _ = os.Getwd()
 	}
 
 	return destFolder

commit 86aea68e9160dd7958721075902b05ace4944c42

Fix a bug which caused the new parser to break
Introduced in: https://github.com/foobar0815/gosignature/commit/355ac4ea22ac48a03cb0ba3bfaf89c2fcf62af40

newparser.go
@@ -8,11 +8,11 @@ import (
 func newParser(fieldMap map[string]string, templateName, tmpl, extension string) (string, error) {
 	buf := new(bytes.Buffer)
 	t, err := template.New(templateName).Delims("[[", "]]").Parse(tmpl)
-	if err == nil {
+	if err != nil {
 		return "", err
 	}
 	err = t.Execute(buf, fieldMap)
-	if err == nil {
+	if err != nil {
 		return "", err
 	}
 	tmpl = buf.String()
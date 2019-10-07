86aea68e9160dd7958721075902b05ace4944c42
foobar0815 <atalanta.bergamo@gmail.com>
2019-10-07 10:10:01 +0200

    Fix a bug which caused the new parser to break
    Introduced in: https://github.com/foobar0815/gosignature/commit/355ac4ea22ac48a03cb0ba3bfaf89c2fcf62af40

--- newparser.go
+++ newparser.go
@@ -8,11 +8,11 @@ import (
 func newParser(fieldMap map[string]string, templateName, tmpl, extension string) (string, error) {
 	buf := new(bytes.Buffer)
 	t, err := template.New(templateName).Delims("[[", "]]").Parse(tmpl)
-	if err == nil {
+	if err != nil {
 		return "", err
 	}
 	err = t.Execute(buf, fieldMap)
-	if err == nil {
+	if err != nil {
 		return "", err
 	}
 	tmpl = buf.String()
